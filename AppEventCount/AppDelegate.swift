46ff80574d1576f521cd0c6f3cc9a96ff5a7ada8
gsprint <sprint@gonzaga.edu>
2020-10-29 15:47:18 -0700

    Finish ADS 4.2 Lab for iOS 13 (remove SceneDelegate)

--- AppEventCount/AppDelegate.swift
+++ AppEventCount/AppDelegate.swift
@@ -10,28 +10,39 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    // following this post to remove the SceneDelegate
+    // https://stackoverflow.com/questions/58084127/ios-13-swift-set-application-root-view-controller-programmatically-does-not
+    var window: UIWindow?
+    var viewController: ViewController?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        viewController = window?.rootViewController as? ViewController
+        viewController?.didLaunchCount += 1
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        viewController?.willResignActiveCount += 1
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        viewController?.didEnterBackgroundCount += 1
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        viewController?.willEnterForegroundCount += 1
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        viewController?.didBecomeActiveCount += 1
+        viewController?.updateView()
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func applicationWillTerminate(_ application: UIApplication) {
+        viewController?.willTerminateCount += 1
     }
-
-
 }
 
--- AppEventCount/ViewController.swift
+++ AppEventCount/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var didFinishLaunchingLabel: UILabel!
+    var didLaunchCount = 0
+
+    @IBOutlet var willResignActiveLabel: UILabel!
+    var willResignActiveCount = 0
+
+    @IBOutlet var didEnterBackgroundLabel: UILabel!
+    var didEnterBackgroundCount = 0
+
+    @IBOutlet var willEnterForegroundLabel: UILabel!
+    var willEnterForegroundCount = 0
+
+    @IBOutlet var didBecomeActiveLabel: UILabel!
+    var didBecomeActiveCount = 0
+
+    @IBOutlet var willTerminateLabel: UILabel!
+    var willTerminateCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateView()
     }
 
+    func updateView() {
+        didFinishLaunchingLabel.text = "The app launched \(didLaunchCount) time(s)"
+        willResignActiveLabel.text = "The app will resign active \(willResignActiveCount) time(s)"
+        didEnterBackgroundLabel.text = "The app did enter background \(didEnterBackgroundCount) time(s)"
+        willEnterForegroundLabel.text = "The app will enter foreground \(willEnterForegroundCount) time(s)"
+        didBecomeActiveLabel.text = "The app did become active \(didBecomeActiveCount) time(s)"
+        willTerminateLabel.text = "The app will terminate \(willTerminateCount) time(s)"
+    }
 
 }
 
